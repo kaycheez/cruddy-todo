822f39efc7a3a8819a0b4649dec31fc1b66a2f92
kaycheez <kevin.m.chau@gmail.com>
2018-08-22 15:23:55 -0700

    Pass getNextUniqueId tests

--- datastore/counter.js
+++ datastore/counter.js
@@ -11,11 +11,11 @@ var counter = 0;
 // Wikipedia entry on Leading Zeros and check out some of code links:
 // https://www.google.com/search?q=what+is+a+zero+padded+number%3F
 
-const zeroPaddedNumber = (num) => {
+const zeroPaddedNumber = num => {
   return sprintf('%05d', num);
 };
 
-const readCounter = (callback) => {
+const readCounter = callback => {
   fs.readFile(exports.counterFile, (err, fileData) => {
     if (err) {
       callback(null, 0);
@@ -27,9 +27,9 @@ const readCounter = (callback) => {
 
 const writeCounter = (count, callback) => {
   var counterString = zeroPaddedNumber(count);
-  fs.writeFile(exports.counterFile, counterString, (err) => {
+  fs.writeFile(exports.counterFile, counterString, err => {
     if (err) {
-      throw ('error writing counter');
+      throw 'error writing counter';
     } else {
       callback(null, counterString);
     }
@@ -38,12 +38,14 @@ const writeCounter = (count, callback) => {
 
 // Public API - Fix this function //////////////////////////////////////////////
 
-exports.getNextUniqueId = () => {
-  counter = counter + 1;
-  return zeroPaddedNumber(counter);
+exports.getNextUniqueId = callback => {
+  readCounter((error, number) => {
+    counter = number + 1;
+    writeCounter(counter, (error, counterString) => {
+      callback(null, counterString);
+    });
+  });
 };
-
-
 
 // Configuration -- DO NOT MODIFY //////////////////////////////////////////////
 
